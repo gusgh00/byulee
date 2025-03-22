7d2a93b9c25fb34ea9b14b448bc17adaf8ba26d2
HYNOBYTE <hyno0913@naver.com>
2025-03-23 01:07:12 +0900

    디데이 카운트

--- src/app/counter/page.tsx
+++ src/app/counter/page.tsx
@@ -1,13 +1,14 @@
 "use client"
 import {useEffect, useState} from "react";
 import dayjs from "dayjs";
-import {addDays, addYears, differenceInDays, isPast, isToday} from "date-fns";
+import {addDays, addYears, differenceInDays, isPast, isThisWeek, isToday} from "date-fns";
 import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
 import Crossfire from "react-canvas-confetti/dist/presets/crossfire";
 import Pride from "react-canvas-confetti/dist/presets/pride";
 
 export default function Counter() {
     const [dDayStatus, setDDayStatus] = useState(false)
+    const [dWeekStatus, setDWeekStatus] = useState(false)
 
     const date = new Date()
     const TBroadcastDay = new Date(2024, 10, 17)
@@ -53,6 +54,9 @@ export default function Counter() {
         if (isToday(birthDay) || isToday(broadcastDay) || isToday(firstDay) || isToday(secondDay) || isToday(thirdDay) || isToday(fourthDay)) {
             setDDayStatus(true)
         }
+        if (isThisWeek(birthDay) || isThisWeek(broadcastDay) || isThisWeek(firstDay) || isThisWeek(secondDay) || isThisWeek(thirdDay) || isThisWeek(fourthDay)) {
+            setDWeekStatus(true)
+        }
     }, []);
 
     return (
@@ -61,19 +65,19 @@ export default function Counter() {
                 <div className="counter_main">
                     <div className="counter_section">
                         {/*생일*/}
-                        <div className={"counter_card " + (isToday(birthDay) ? "active" : "")}>
+                        <div className={"counter_card " + (isThisWeek(birthDay) ? "active" : "")}>
                             <span className="DOL default_text counter_name">생일까지...</span>
                             <span className="DOL grey_text counter_date">{"(" + dayjs(birthDay).format("YYYY년 MM월 DD일") + ")"}</span>
                             <span className="font_01 default_text counter_num">{"D-" + (isToday(birthDay) ? "DAY" : differenceInDays(birthDay, date) + 1)}</span>
                         </div>
                         {/*방송 1주년*/}
-                        <div className={"counter_card " + (isToday(broadcastDay) ? "active" : "")}>
+                        <div className={"counter_card " + (isThisWeek(broadcastDay) ? "active" : "")}>
                             <span className="DOL default_text counter_name">방송 1주년까지...</span>
                             <span className="DOL grey_text counter_date">{"(" + dayjs(broadcastDay).format("YYYY년 MM월 DD일") + ")"}</span>
                             <span className="font_01 default_text counter_num">{"D-" + (isToday(broadcastDay) ? "DAY" : differenceInDays(broadcastDay, date) + 1)}</span>
                         </div>
                         {/*방송 200일*/}
-                        <div className={"counter_card " + (isToday(firstDay) ? "active" : "")}>
+                        <div className={"counter_card " + (isThisWeek(firstDay) ? "active" : "")}>
                             <span className="DOL default_text counter_name">방송 {firstCnt}일까지...</span>
                             <span className="DOL grey_text counter_date">{"(" + dayjs(firstDay).format("YYYY년 MM월 DD일") + ")"}</span>
                             <span className="font_01 default_text counter_num">{"D-" + (isToday(firstDay) ? "DAY" : differenceInDays(firstDay, date) + 1)}</span>
@@ -81,27 +85,27 @@ export default function Counter() {
                     </div>
                     <div className="counter_section">
                         {/*방송 300일*/}
-                        <div className={"counter_card " + (isToday(secondDay) ? "active" : "")}>
+                        <div className={"counter_card " + (isThisWeek(secondDay) ? "active" : "")}>
                             <span className="DOL default_text counter_name">방송 {secondCnt}일까지...</span>
                             <span className="DOL grey_text counter_date">{"(" + dayjs(secondDay).format("YYYY년 MM월 DD일") + ")"}</span>
                             <span className="font_01 default_text counter_num">{"D-" + (isToday(secondDay) ? "DAY" : differenceInDays(secondDay, date) + 1)}</span>
                         </div>
                         {/*방송 400일*/}
-                        <div className={"counter_card " + (isToday(thirdDay) ? "active" : "")}>
+                        <div className={"counter_card " + (isThisWeek(thirdDay) ? "active" : "")}>
                             <span className="DOL default_text counter_name">방송 {thirdCnt}일까지...</span>
                             <span className="DOL grey_text counter_date">{"(" + dayjs(thirdDay).format("YYYY년 MM월 DD일") + ")"}</span>
                             <span className="font_01 default_text counter_num">{"D-" + (isToday(thirdDay) ? "DAY" : differenceInDays(thirdDay, date) + 1)}</span>
                         </div>
                         {/*방송 500일*/}
-                        <div className={"counter_card " + (isToday(fourthDay) ? "active" : "")}>
+                        <div className={"counter_card " + (isThisWeek(fourthDay) ? "active" : "")}>
                             <span className="DOL default_text counter_name">방송 {fourthCnt}일까지...</span>
                             <span className="DOL grey_text counter_date">{"(" + dayjs(fourthDay).format("YYYY년 MM월 DD일") + ")"}</span>
                             <span className="font_01 default_text counter_num">{"D-" + (isToday(fourthDay) ? "DAY" : differenceInDays(fourthDay, date) + 1)}</span>
                         </div>
                     </div>
-                    {dDayStatus ? <Crossfire autorun={{ speed: 3 }} /> : null}
-                    {dDayStatus ? <Fireworks autorun={{ speed: 1 }} /> : null}
-                    {dDayStatus ? <Pride autorun={{ speed: 2 }} /> : null}
+                    {dWeekStatus ? <Crossfire autorun={{ speed: dDayStatus ? 7 : 3 }} /> : null}
+                    {dWeekStatus ? <Fireworks autorun={{ speed: dDayStatus ? 7 : 1 }} /> : null}
+                    {dWeekStatus ? <Pride autorun={{ speed: dDayStatus ? 5 : 2 }} /> : null}
                 </div>
             </div>
         </div>
